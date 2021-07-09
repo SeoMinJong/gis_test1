e2895006805f1d5ed527b20541f07b7368a12944
SeoMinJong <tjalswhd113@naver.com>
2021-07-09 10:17:15 +0900

    7/9 gis_test1 commit 2

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render
 
 # Create your views here.
 
+# request 안에 모든 전달 데이터가 들어있다.
 def Hello_World(request):
-    return render(request, 'accountapp/hello_world.html')
+    if request.method == "POST":
+        return render(request, 'accountapp/hello_world.html', context={'text': 'POST METHOD!'})
+    else:
+        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!'})
