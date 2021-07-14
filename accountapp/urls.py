5198a29a755b2550436d2813f024fa31f3bb3e7c
SeoMinJong <tjalswhd113@naver.com>
2021-07-14 14:49:42 +0900

    7/14 gis_test1 commit 1

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from accountapp.views import Hello_World
+from accountapp.views import Hello_World, AccountCreateView
 
 app_name = 'accountapp'
 
 urlpatterns = [ # 어떤 패턴으로 라우팅을 해줄수 있는지를 입력해주는 곳
-    path('Hello_World/', Hello_World, name='Hello_World') # 경로지정('홈페이지의 경로/', 실제 파일 내부의 함수 이름, name='지정할 이름')
+    path('Hello_World/', Hello_World, name='Hello_World'), # 경로지정('홈페이지의 경로/', 실제 파일 내부의 함수 이름, name='지정할 이름')
+    path('Create/', AccountCreateView.as_view, name='create') # class이기 때문에 바로 사용할 수 없고 as_view로 함수를 뱉어줘야한다.
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,9 +1,14 @@
-from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
 
 # request 안에 모든 전달 데이터가 들어있다.
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
+
 from accountapp.models import NewModel
 
 
@@ -16,12 +21,21 @@ def Hello_World(request):
         model_instance.text = temp   # 데이터베이스에 만들어뒀던 text변수에 넘겨준다.
         model_instance.save()        # 실제로 데이터베이스에 저장해주는 것
 
-        data_list = NewModel.objects.all() # NewModel이라는 데이터베이스에 있는 데이터들을 다 가져오는 법
-
-        return render(request, 'accountapp/hello_world.html', context={'data_list': data_list})
-        # 받아온 텍스트를 넘겨줘서 출력할 수 있도록 한다.
-        # model_instance를 만들어서 넘겨준다.
+        # data_list = NewModel.objects.all() # NewModel이라는 데이터베이스에 있는 데이터들을 다 가져오는 법
+        #
+        # return render(request, 'accountapp/hello_world.html', context={'data_list': data_list})
+        # # 받아온 텍스트를 넘겨줘서 출력할 수 있도록 한다.
+        # # model_instance를 만들어서 넘겨준다.
+        return HttpResponseRedirect(reverse('accountapp:Hello_World'))
+        # 해당하는 app안에 있는 app_name으로 app의 위치를 알려주고 그 안에 있는 라우팅을 가져온다.
+        # 다만 이때 HttpResponseRedirect 안에 인자는 주소가 들어가야 하기 때문에 reverse로 주소를 불러온다.
     else:
         data_list = NewModel.objects.all()  # NewModel이라는 데이터베이스에 있는 데이터들을 다 가져오는 법
 
         return render(request, 'accountapp/hello_world.html', context={'data_list': data_list})
+
+class AccountCreateView(CreateView):
+    model = User # User를 만드는 것
+    form_class =UserCreationForm # User의 정보
+    success_url = reverse_lazy('accountapp:Hello_World') # reverse_lazy = class에서 주소를 불러올 때 사용되는 함수
+    template_name = 'accountapp/create.html'
