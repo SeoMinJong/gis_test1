5b68c98d9f3370f7ea0e8db0343f091de1ea3882
SeoMinJong <tjalswhd113@naver.com>
2021-07-27 10:29:38 +0900

    7/27 gis_test1 commit 3

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -5,6 +5,6 @@ from profileapp.views import ProfileCreateView
 app_name = 'profileapp'
 
 urlpatterns = [
-    path('create/', ProfileCreateView.as_view(), name='create')
+    path('create/', ProfileCreateView.as_view(), name='create'),
     # 메인앱에서 분기를 했기 때문에 profileapp/create로 들어가지기 때문에 나눠진다.
 ]
--- profileapp/views.py
+++ profileapp/views.py
@@ -13,3 +13,9 @@ class ProfileCreateView(CreateView):
     form_class = ProfileCreationForm
     success_url = reverse_lazy('accountapp:Hello_World')
     template_name = 'profileapp/create.html'
+
+    def form_valid(self, form): # form = 클라이언트에게 요청을 받아 입력받은 데이터 (image, nickname, )
+        form.instance.user = self.request.user
+        # form은 Profile안에 있는 form만을 가져오는 것이기 때문에 user가 존재하지 않는다 고로 그 form을 가지고있는 instance객체를 접근해서 가져와야하고
+        # request 즉, 요청을 받은 user의 아이디를 넣어주는 것이다.
+        return super().form_valid(form)
