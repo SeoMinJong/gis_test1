1a1323d3d6180ced61679829d56bcf89710b08f6
SeoMinJong <tjalswhd113@naver.com>
2021-07-27 09:46:33 +0900

    7/27 gis_test1 commit 2

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path, include
 
-urlpatterns = [
+from profileapp.views import ProfileCreateView
 
+app_name = 'profileapp'
+
+urlpatterns = [
+    path('create/', ProfileCreateView.as_view(), name='create')
+    # 메인앱에서 분기를 했기 때문에 profileapp/create로 들어가지기 때문에 나눠진다.
 ]
