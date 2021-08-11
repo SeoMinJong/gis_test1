b4c5e57c23136474844d6f32df460a86f5bf2211
SeoMinJong <tjalswhd113@naver.com>
2021-08-11 14:48:01 +0900

    8/10 gis_test1 commit 3

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
 
 # 장고에서 제공해주는 기본 모델(models.Model)을 상속을 받아 와준다.
 class NewModel(models.Model):
-    text = models.CharField(max_length=255, null=False) # 최대 문자열 길이(max_length, null을 허용하지 않음(null=False)
+    text = models.CharField(max_length=255, null=False)  # 최대 문자열 길이(max_length, null을 허용하지 않음(null=False)
