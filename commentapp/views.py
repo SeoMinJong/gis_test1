9e364d3cd03af285f4e97e7a069b4e318b9160ff
SeoMinJong <tjalswhd113@naver.com>
2021-08-11 16:18:12 +0900

    8/11 gis_test1 commit 4 (ngrok.exe + ALLOWED_HOSTS setting)

--- commentapp/views.py
+++ commentapp/views.py
@@ -29,7 +29,7 @@ class CommentCreateView(CreateView):
 
 
 @method_decorator(comment_ownership_required, 'get')
-@method_decorator(comment_ownership_required, 'get')
+@method_decorator(comment_ownership_required, 'post')
 class CommentDeleteView(DeleteView):
     model = Comment
     context_object_name = 'target_comment'
