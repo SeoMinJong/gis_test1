0435c7439d8e60a0e91d80c895b8fb120ac553be
SeoMinJong <tjalswhd113@naver.com>
2021-08-17 10:13:49 +0900

    8/17 gis_test1 commit 1 projectapp detail 생성, commentapp decorators.py 수정

--- commentapp/decorators.py
+++ commentapp/decorators.py
@@ -7,7 +7,7 @@ def comment_ownership_required(func):
     def decorated(request, *args, **kwargs):
         target_comment = Comment.objects.get(pk=kwargs['pk'])
         if target_comment.writer == request.user:
-            return decorated(request, *args, **kwargs)
+            return func(request, *args, **kwargs)
         else:
             return HttpResponseForbidden()
 
